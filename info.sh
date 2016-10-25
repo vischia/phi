f3bf7528d25709b1b22b9d79a06764666e7e08ff
Pietro Vischia <pietro.vischia@gmail.com>
2016-10-25 23:35:50 +0200

    More info in info.sh

--- info.sh
+++ info.sh
@@ -5,3 +5,12 @@
 
 # Extended information
 micsmc --verbose -a
+
+
+# The IP of the microprocessors (standard from the https://software.intel.com/sites/default/files/managed/26/d6/Intel_Xeon_Phi_Quick_Start_Developers_Guide-MPSS-3.4.pdf )
+
+echo "Mic0 should be seen by the host as IP 172.31.1.1"
+echo "Mic1 should be seen by the host as IP 172.31.2.1"
+echo "Host should be seen by Mic0 as IP 172.31.1.254"
+echo "Host should be seen by Mic1 as IP 172.31.2.254"
+
